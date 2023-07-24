bd0552d87472fe6bc874ddf541ac8f932c98f156
Rico <96888953+KopfdesDaemons@users.noreply.github.com>
2023-07-24 18:33:20 +0200

    author, tags, categories, word cloud

--- single.php
+++ single.php
@@ -12,24 +12,79 @@
                     <h1 class="title"><?php the_title(); ?></h1>
                 </header>
                 <span><?php the_date(); ?></span>
+                <div class="post-categories">
+                    <?php
+                        $categories = get_the_category();
+                        if (!empty($categories)) {
+                            echo '<ul>';
+                            foreach ($categories as $category) {
+                                echo '<li><a href="' . esc_url(get_category_link($category->term_id)) . '">' . $category->name . '</a></li>';
+                            }
+                            echo '</ul>';
+                        }
+                        ?>
+                </div>
+
                 <?php the_content(); ?>
-                <?php echo theme_slug_social_sharing(); ?>
+
+                <!-- Tags -->
+                <div class="post-tags">
+                    <?php
+                        $tags = get_the_tags();
+                        if (!empty($tags)) {
+                            echo '<ul>';
+                            foreach ($tags as $tag) {
+                                echo '<li><a href="' . esc_url(get_tag_link($tag->term_id)) . '">' . $tag->name . '</a></li>';
+                            }
+                            echo '</ul>';
+                        }
+                        ?>
+                </div>
+
+
+                <!-- Author -->
                 <?php
-                // Kommentare anzeigen
+                    // Autorendaten abrufen
+                    $author_id = get_the_author_meta('ID');
+                    $author_name = get_the_author_meta('display_name');
+                    $author_description = get_the_author_meta('description');
+                    $author_website = get_the_author_meta('user_url');
+
+                    // Avatar des Autors
+                    $author_avatar = get_avatar($author_id, 80); // 96 ist die Größe des Avatars in Pixeln
+
+                    ?>
+                <div class="author-info">
+                    <div class="author-avatar">
+                        <?php echo $author_avatar; ?>
+                    </div>
+                    <div class="author-details">
+                        <div class="author-row">
+                            <h3><a
+                                    href="<?php echo get_author_posts_url($author_id); ?>"><?php echo $author_name; ?></a>
+                            </h3>
+                            <?php if ($author_website) : ?>
+                            <a href="<?php echo $author_website; ?>" target="_blank">🌐</a>
+                            <?php endif; ?>
+                        </div>
+                        <p><?php echo $author_description; ?></p>
+                    </div>
+                </div>
+
+                <!-- Social Sharing -->
+                <?php echo theme_slug_social_sharing(); ?>
+
+                <!-- Kommentare -->
+                <?php
                 if (comments_open() || get_comments_number()) {
                     comments_template();
                 }
             endwhile;
                 ?>
             </div>
+
             <?php get_sidebar(); ?>
         </div>
-
-        <footer class="entry-footer">
-            <!-- Weitere Inhalte wie Kategorien, Tags, Autor, Datum usw. -->
-        </footer>
     </article>
-
 </main>
-
 <?php get_footer(); ?>
--- template-parts/feed.php
+++ template-parts/feed.php
@@ -3,20 +3,20 @@ function display_post_card($post_classes)
 {
     ob_start(); // Start output buffering
 ?>
-<div class="<?php echo implode(' ', $post_classes); ?>">
-    <?php if (has_post_thumbnail()) { ?>
-    <a class="imgA" href="<?php the_permalink(); ?>">
-        <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>">
-    </a>
-    <?php } ?>
-    <div class="textDiv">
-        <a href="<?php the_permalink(); ?>">
-            <h2><?php the_title(); ?></h2>
-        </a>
-        <span class="date"><?php the_date(); ?></span>
-        <?php the_excerpt(); ?>
-        <div class="tagsDiv">
-            <?php
+    <div class="<?php echo implode(' ', $post_classes); ?>">
+        <?php if (has_post_thumbnail()) { ?>
+            <a class="imgA" href="<?php the_permalink(); ?>">
+                <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>">
+            </a>
+        <?php } ?>
+        <div class="textDiv">
+            <a href="<?php the_permalink(); ?>">
+                <h2><?php the_title(); ?></h2>
+            </a>
+            <span class="date"><?php the_date(); ?></span>
+            <?php the_excerpt(); ?>
+            <div class="tagsDiv">
+                <?php
                 $tags = get_the_tags();
                 if ($tags) {
                     echo '<ul>';
@@ -27,18 +27,21 @@ function display_post_card($post_classes)
                     echo '</ul>';
                 }
                 ?>
-        </div>
-        <div class="buttomDiv">
-            <a href="<?php comments_link(); ?>" class="commentscount">
-                <?php
-                    $commentscount = get_comments_number();
-                    echo '<span>' . $commentscount . '</span>' . '<span>💬</span>';
-                    ?>
-            </a>
-            <a class="readMore" href="<?php the_permalink(); ?>">read more</a>
+            </div>
+            <div class="buttomDiv">
+                <span class="pin">📌</span>
+                <div class="postLinks">
+                    <a href="<?php comments_link(); ?>" class="commentscount">
+                        <?php
+                        $commentscount = get_comments_number();
+                        echo '<span>' . $commentscount . '</span>' . '<span>💬</span>';
+                        ?>
+                    </a>
+                    <a class="readMore" href="<?php the_permalink(); ?>">read more</a>
+                </div>
+            </div>
         </div>
     </div>
-</div>
 <?php
     return ob_get_clean(); // Return the buffered output as a string
 }
