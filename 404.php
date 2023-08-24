cac9f8b8b1e4b68b34ac066343b79304a6fe88f6
Rico <96888953+KopfdesDaemons@users.noreply.github.com>
2023-08-25 00:59:12 +0200

    404 page fix; mobile content padding

--- 404.php
+++ 404.php
@@ -1,12 +1,12 @@
 <?php get_header(); ?>
 <main role="main">
-    <section class="spacer grid">
+    <section class="spacer">
         <div class="content error">
             <h1><?php esc_html_e('File not found', 'my-theme'); ?></h1>
             <span>😥</span>
-            <a href="<?php echo esc_url(home_url('/')); ?>"><?php esc_html_e('Go to the home page', 'my-theme'); ?></a>
+            <a href="<?php echo esc_url(home_url('/')); ?>"><?php echo __('Go to the home page', 'my-theme'); ?></a>
         </div>
-        <?php get_sidebar(); ?>
+        <!-- <?php get_sidebar(); ?> -->
     </section>
 </main>
 <?php get_footer(); ?>
